ea39e322d916b48d2101136e5024be2776532b9f
Jonathan Amiez <jonathan.amiez@gmail.com>
2014-03-18 00:08:35 +0100

    tmp

--- lib/lita/handlers/nagios.rb
+++ lib/lita/handlers/nagios.rb
@@ -4,33 +4,57 @@ module Lita
   module Handlers
     class Nagios < Handler
 
+      SERVICE_STATES = Hash.new(:clear).merge({
+        "OK" => :light_green,
+        "WARNING" => :yellow,
+        "CRITICAL" => :light_red,
+        "UNKNOWN" => :orange,
+      })
+
+      HOST_STATES = Hash.new(:clear).merge({
+        "UP" => :light_green,
+        "DOWN" => :light_red,
+        "UNREACHABLE" => :orange,
+      })
+
+      NOTIF_TYPES = Hash.new(:clear).merge({
+        "PROBLEM" => :light_red,
+        "RECOVERY" => :light_green,
+        "ACKNOWLEDGEMENT" => :yellow,
+#        "FLAPPINGSTART",
+#        "FLAPPINGSTOP",
+#        "FLAPPINGDISABLED",
+#        "DOWNTIMESTART",
+#        "DOWNTIMESTOP",
+#        "DOWNTIMECANCELLED",
+      })
+
       def self.default_config(config)
         config.default_room = nil
       end
 
-      http.post "/lita/nagios", :receive
+      http.post "/nagios/alerts", :alerts
 
-      def receive(request, response)
-        if request.params.has_key?("type")
-          notif_type = request.params["type"].to_sym
-          unless respond_to?(notif_type, true)
-            raise "'#{notif_type}' is not supported by Nagios handler"
-          end
+      def alerts(request, response)
+        params = request.params.dup
+
+        if params.has_key?("type")
+          notif_type = params["type"].to_sym
+          raise "'#{notif_type}' is not supported by Nagios handler" unless respond_to?(notif_type, true)
         else
           raise "Notification type must be defined in Nagios command"
         end
 
-        if request.params.has_key?("room")
-          room = request.params["room"]
+        if params.has_key?("room")
+          room = params["room"]
         elsif Lita.config.handlers.nagios.default_room
           room = Lita.config.handlers.nagios.default_room
         else
           raise "Room must be defined. Either fix your command or specify a default room ('config.handlers.nagios.default_room')"
         end
 
-        message = send(notif_type, request.params)
-
-        ack = request.params["notificationtype"] == "ACKNOWLEDGEMENT" ? irc_color("[ACK] ", :light_green) : ""
+        message = notification_type(params)
+        message += send(notif_type, params)
 
         target = Source.new(room: room)
         robot.send_message(target, "nagios: #{ack}#{message}")
@@ -40,38 +64,21 @@ module Lita
 
       private
 
-      def irc_color(str, fg, bg=nil)
-        str
+      def notification_type(params)
+        color = NOTIF_TYPES[params["notificationtype"]]
+        "[#{params["notificationtype"]}] ".irc_color(color)
       end
 
       def host(params)
-        case params["state"]
-        when "UP"
-          color = :light_green
-        when "DOWN"
-          color = :light_red
-        when "UNREACHABLE"
-          color = :orange
-        end
-        status = irc_color(params["state"], color)
-
-        message = "#{params["host"]} is #{status}: #{params["output"]}"
+        color = HOST_STATES[params["hoststate"]]
+        status = params["state"].irc_color(color)
+        "#{params["host"].irc_color(:orange)} is #{status}: #{params["output"].gsub(/\s*#{params["state"]}\s*:?\s*/, "")}"
       end
 
       def service(params)
-        case params["state"]
-        when "OK"
-          color = :light_green
-        when "WARNING"
-          color = :yellow
-        when "CRITICAL"
-          color = :light_red
-        when "UNKNOWN"
-          color = :orange
-        end
-        status = irc_color(params["state"], color)
-
-        message = "#{params["host"]}:#{params["description"]} is #{status}: #{params["output"]}"
+        color = SERVICE_STATES[params["servicestate"]]
+        status = params["state"].irc_color(color)
+        "#{params["host"].irc_color(:orange)}:#{params["description"]} is #{status}: #{params["output"].gsub(/\s*#{params["state"]}\s*:?\s*/, "")}"
       end
     end
 
