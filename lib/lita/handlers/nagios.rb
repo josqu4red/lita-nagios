42db648080f26828ebba0a9e66ef675e9dc78c64
Jonathan Amiez <jonathan.amiez@gmail.com>
2014-10-13 16:59:30 +0200

    handle only problem, recovery and ack. remove dummy color support

--- lib/lita/handlers/nagios.rb
+++ lib/lita/handlers/nagios.rb
@@ -157,61 +157,40 @@ module Lita
       http.post "/nagios/notifications", :receive
 
       def receive(request, response)
-        if request.params.has_key?("type")
-          notif_type = request.params["type"].to_sym
-          unless respond_to?(notif_type, true)
-            raise "'#{notif_type}' is not supported by Nagios handler"
-          end
-        else
-          raise "Notification type must be defined in Nagios command"
-        end
+        params = request.params
 
-        if request.params.has_key?("room")
-          room = request.params["room"]
+        if params.has_key?("room")
+          room = params["room"]
         elsif config.default_room
           room = config.default_room
         else
           raise "Room must be defined. Either fix your command or specify a default room ('config.handlers.nagios.default_room')"
         end
 
-        message = send(notif_type, request.params)
+        message = nil
+        case params["notificationtype"]
+        when "ACKNOWLEDGEMENT"
+          message = "[ACK] "
+        when "PROBLEM", "RECOVERY"
+          message = ""
+        else
+          # Don't process FLAPPING* and DOWNTIME* events for now
+          return
+        end
+
+        case params["type"]
+        when "service"
+          message += "#{params["host"]}:#{params["description"]} is #{params["state"]}: #{params["output"]}"
+        when "host"
+          message += "#{params["host"]} is #{params["state"]}: #{params["output"]}"
+        else
+          raise "Notification type must be defined in Nagios command ('host' or 'service')"
+        end
 
         target = Source.new(room: room)
         robot.send_message(target, "nagios: #{message}")
       rescue Exception => e
         Lita.logger.error(e)
-      end
-
-      private
-
-      def host(params)
-        case params["state"]
-        when "UP"
-          color = :light_green
-        when "DOWN"
-          color = :light_red
-        when "UNREACHABLE"
-          color = :orange
-        end
-        status = params["state"]
-
-        message = "#{params["host"]} is #{status}: #{params["output"]}"
-      end
-
-      def service(params)
-        case params["state"]
-        when "OK"
-          color = :light_green
-        when "WARNING"
-          color = :yellow
-        when "CRITICAL"
-          color = :light_red
-        when "UNKNOWN"
-          color = :orange
-        end
-        status = params["state"]
-
-        message = "#{params["host"]}:#{params["description"]} is #{status}: #{params["output"]}"
       end
     end
 
